b29687d9a5b6eb857822ea6aebff5282bb009240
moustafaElhadary <moustafaElhadary96@gmail.com>
2022-07-15 15:06:44 +0200

    BG color change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -44,7 +44,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           sizes="32x32"
         />
         <link rel="apple-touch-icon" href="/apple-icon.png"></link>
-        <meta name="theme-color" content="#317EFB" />
+        <meta name="theme-color" content="#fff" />
       </Head>
       <Provider store={store}>
         <SWRConfig
